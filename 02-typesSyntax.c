eae60479a6e492df2d4a4c7766b1fa7d02647865
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-01-02 20:27:52 +0500

    Format specifiers and  Syntax Types

---
+++ 02-typesSyntax.c
@@ -0,0 +1,17 @@
+//M-AbrarUlHaq-C-language.cpp
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int main() {
+    system("cls");
+    printf("\n%c", 'a');
+    printf("\n%s", "Iqra University");
+    printf("\n%d", 20);
+    printf("\n%f", 35.5);
+    printf("\n%d", 123456789);
+    //*Single Line
+    printf("\n%c %s %d %f %ld", 'a', "Iqra University", 20,35.5,134567);
+
+return 0;
+}
