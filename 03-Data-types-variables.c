af87f3efc4d3470dfd4a7a06216eae79643a260a
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-01-03 18:21:38 +0500

    Data types variables in C

---
+++ 03-Data-types-variables.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+//#include<conio.h>
+//#include<stdlib.h>
+
+int main() {
+    //systam("cls");
+    /*
+    char a = 'a';
+    int b = 12;
+    float c = 12.5;
+    double d = 1234567;
+    printf("%c %d %f %lf", a, b, c, d);
+    */
+   //************************************************************* */
+   /*
+   char a = 'a', a1 = 'b';
+   int b= 12, b1 = 13;
+   float c = 12.5, c1 = 13.5;
+   double d = 1234567, d1 = 1234568;
+   printf("\n%c %d %f %1f", a, b, c, d);
+   printf("\n%c %d %f %1f", a1, b1, c1, d1);
+   */
+   //************************************************************* */
+   
+    char a = 97;
+    int b = 'A';
+    float c = 12.5;
+    double d = 1234567;
+    printf("%c %d %f %lf", a, b, c, d);
+//M-AbrarUlHaq-C-language.cpp
+
+   return 0;
+
+}
---
+++ 03-assignment.c
@@ -0,0 +1,19 @@
+//M-AbrarUlHaq-C-language.cpp
+#include<stdio.h>
+int main () {
+	char a= 'A';
+	int id = 0507;
+	float gpa = 2.5;
+	double marks= 50.00007;
+	printf("%c\n", a);
+	printf("%d\n", id);
+	printf("%f\n", gpa);
+	printf("%lf\n", marks);
+    printf("Name: Muhammad Abrar ul haq\nID: IUBS-0225-0507");
+	printf("\nProgram: BSCS");
+	printf("\nAge: 22\nGPA: 2");
+
+   return 0;
+}
+	 
+ 
