76cc1d57e5da8ca74ff416853d6281494b897f38
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-01-04 11:32:49 +0500

    User input in C

--- 01-assignment.c
+++ 01-assignment.c
@@ -1,11 +1,12 @@
-#include<conio.h>
-#include<stdio.h>
-#include<stdlib.h>
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+int main()
+{
     printf("Name: Muhammad Abrar ul haq\nID: IUBS-0225-0507");
     printf("\nProgram: BSCS");
     printf("\nAge: 22\nGPA: 2");
 
-return 0;
+    return 0;
 }
---
+++ 04-user-input.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+// #include<conio.h>
+// #include<stdlib.h>
+
+int main()
+{
+    /*
+    char a;
+    int b;
+    float c;
+    double d;
+
+    printf("\nEnter Character: ");
+    scanf("%c", &a);
+
+    printf("\nEnter Integer: ");
+    scanf("%d", &b);
+
+    printf("\nEnter Float: ");
+    scanf("%f", &c);
+
+    printf("\nEnter Double: ");
+    scanf("%lf", &d);
+    printf("\n%c %d %f %lf", a, b, c, d);
+
+    */
+
+    /*
+     char a;
+     int b;
+     float c;
+     double d;
+
+     printf("\nEnter Char Integer Float Double\n:");
+     scanf("%c %d %f %lf", &a, &b, &c, &d);
+     printf("\n%c %d %f %lf", a, b, c, d);
+ */
+    system("cls");
+    printf("\nWhat is your section:");
+    getche();
+
+    return 0;
+}
