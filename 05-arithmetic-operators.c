68e8bdefb9635e0b97ba2e2248342b4dbd29b735
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-01-05 15:10:11 +0500

    Arithmetic operators

---
+++ 05-arithmetic-operators.c
@@ -0,0 +1,43 @@
+int main()
+{
+    /*
+    float a = 45, b = 1, sn, cs, tn, snh, csh, tnh;
+    sn = sin(a);
+    cs = cos(a);
+    tn = tan(a);
+
+    snh = sinh(b);
+    csh = cosh(b);
+    tnh = tanh(b);
+
+    printf("\n\n\n Trignometric Functions");
+    printf("\n Sin 45 %.2f", sn);
+    printf("\n Cos 45 = %.2f", cs);
+    printf("\n Tan 45 %.2f", tn);
+    printf("\n\n\n Hyperbolic Functions");
+    printf("\n SanH 1 %.2f", snh);
+    printf("\n CoSH 1 %.2f", csh);
+    printf("\n TanH 1 = %.2f", tnh);
+
+    */
+    float a = 45, b = 1, sn, cs, tn, snh, csh, tnh;
+    sn = sin(a);
+    cs = cos(a);
+    tn = tan(a);
+    snh = sinh(b);
+    csh = cosh(b);
+    tnh = tanh(b);
+    printf("\n\n\n--Trignometric Functions--");
+    printf("\n Sin 45%.2f", sn);
+    printf("\n Cos 45 = %.2f", cs);
+    printf("\n Tan 45 %.2f", tn);
+    printf("\n\n\n--Hyperbolic Functions--");
+    printf("\n SanH 1 = %.2f", snh);
+    printf("\n CoSH 1 %.2f", csh);
+    printf("\n TanH 1 = %.2f", tnh);
+    printf("\n\n\--Math Functions--");
+    printf("\n pow 2.3 %.2f", pow(2, 3));
+    printf("\n sqrt 2.3 %.2f", sqrt(49));
+
+    return 0;
+}
