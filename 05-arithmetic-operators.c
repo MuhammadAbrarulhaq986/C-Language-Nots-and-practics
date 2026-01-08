15a803e0beb755fe22b307452cf13d373f887b1b
Muhammad Abrar ul haq <ulhaqabrar106@gmail.com>
2026-01-08 09:47:16 +0500

    Arithmetic Operations using increment and decrement oprators

--- 05-arithmetic-operators.c
+++ 05-arithmetic-operators.c
@@ -18,26 +18,69 @@ int main()
     printf("\n SanH 1 %.2f", snh);
     printf("\n CoSH 1 %.2f", csh);
     printf("\n TanH 1 = %.2f", tnh);
-
     */
+
+    /*
     float a = 45, b = 1, sn, cs, tn, snh, csh, tnh;
-    sn = sin(a);
-    cs = cos(a);
-    tn = tan(a);
-    snh = sinh(b);
-    csh = cosh(b);
-    tnh = tanh(b);
-    printf("\n\n\n--Trignometric Functions--");
-    printf("\n Sin 45%.2f", sn);
-    printf("\n Cos 45 = %.2f", cs);
-    printf("\n Tan 45 %.2f", tn);
-    printf("\n\n\n--Hyperbolic Functions--");
-    printf("\n SanH 1 = %.2f", snh);
-    printf("\n CoSH 1 %.2f", csh);
-    printf("\n TanH 1 = %.2f", tnh);
-    printf("\n\n\--Math Functions--");
-    printf("\n pow 2.3 %.2f", pow(2, 3));
-    printf("\n sqrt 2.3 %.2f", sqrt(49));
+     sn = sin(a);
+     cs = cos(a);
+     tn = tan(a);
+     snh = sinh(b);
+     csh = cosh(b);
+     tnh = tanh(b);
+     printf("\n\n\n--Trignometric Functions--");
+     printf("\n Sin 45%.2f", sn);
+     printf("\n Cos 45 = %.2f", cs);
+     printf("\n Tan 45 %.2f", tn);
+     printf("\n\n\n--Hyperbolic Functions--");
+     printf("\n SanH 1 = %.2f", snh);
+     printf("\n CoSH 1 %.2f", csh);
+     printf("\n TanH 1 = %.2f", tnh);
+     printf("\n\n\--Math Functions--");
+     printf("\n pow 2.3 %.2f", pow(2, 3));
+     printf("\n sqrt 2.3 %.2f", sqrt(49));
+ */
+
+    // M-AbrarUlHaq-C-language.cpp
+
+    // system("cls");
+    /*
+    int a = 2, b = 4, c1, c2, c3, c4, d1, d2, d3, d4;
+    c1 = c2 = c3 = c4 = 5;
+    d1 = d2 = d3 = d4 = 8;
+    printf("Arithmetic operations");
+    printf("\n a+b=%d, a-b= %d, a*b= %d, a/b= %d", a + b, a - b, a * b, a / b);
+    printf("\n Relational operations");
+    printf("\n a>b %d, a<b %d, a>=b %d, a<=b %d, a==b %d, a!=b %d", a > b, a < b, a >= b, a <= b, a == b, a != b);
+    printf("\n Assignment operations");
+    printf("\n c1+=%d, c2-= %d, c3*= %d, c4/3 %d", c1 += 3, c2 - 3, c3 = 3, c4 / 3);
+    printf("\n Increment/Decrement operations");
+    printf("\n d1++%d, ++d2 %d, d3--%d, --d4%d", d1++, ++d2, d3--, --d4);
+    */
+    /*
+    int a = 5;
+    printf("\na++%d.\na = %d.", a++, a);
+    printf("\na = %d.", a);
+ */
+    /*
+        int a = 5;
+        printf("\na-- %d, a = %d", a--, a);
+        printf("\na = %d", a);
+    */
+    /*
+     int a = 5;
+     printf("\n--a %d\n a = %d", --a, a);
+     printf("\na = %d", a);
+     */
+    /*
+    int a = 5;
+    printf("\na-=%d\n--a %d\na = %d", a -= 5, --a, a);
+    printf("\na = %d", a);
+    */
+
+    int a = 5;
+    printf("\na += %d\n++a %d\na = %d", a += 5, a++, a);
+    printf("\na = %d", a);
 
     return 0;
 }
